d813120c32f6e6a05af9f93dbebd60e697ce3e37
nartc <nartc7789@gmail.com>
2024-11-05 14:41:13 -0600

    chore: add ngt

--- apps/nx-ng-app/src/app/app.component.ts
+++ apps/nx-ng-app/src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgtCanvas } from 'angular-three';
+import { NgtsOrbitControls } from 'angular-three-soba/controls';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  template: `
+    <ngt-mesh></ngt-mesh>
+    <ngts-orbit-controls />
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgtsOrbitControls],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class SceneGraphComponent {}
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, NgtCanvas],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'nx-ng-app';
+
+  sceneGraph = SceneGraphComponent;
 }
